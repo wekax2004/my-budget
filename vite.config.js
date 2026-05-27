081fc97f2fc6f5769ea984740456315a6202f762
Tal <tal@example.com>
2026-05-27 09:10:33 +0300

    fix: pwa manifest icon paths

--- vite.config.js
+++ vite.config.js
@@ -19,12 +19,12 @@ export default defineConfig({
         display: 'standalone',
         icons: [
           {
-            src: 'icon-192x192.png',
+            src: 'icon.png',
             sizes: '192x192',
             type: 'image/png'
           },
           {
-            src: 'icon-512x512.png',
+            src: 'icon.png',
             sizes: '512x512',
             type: 'image/png'
           }
